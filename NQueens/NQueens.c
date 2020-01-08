4c89e1f81ce83b145b5b4ee9902748b5bdcce59e
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2020-01-08 15:00:17 +0530

    Add files via upload

--- NQueens/NQueens.c
+++ NQueens/NQueens.c
@@ -2,16 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 #define SIZE 20
-int count = 0;
+int count = 0, backTrack = 0;
 
 void display(int board[], int n) {
-	printf("\n\n\nSOLUTION NO. '%d':\n\n\n",++count);
-	for (int i=1; i<=n; i++)
+	int i,j;
+	printf("\n\n\nSOLUTION NUMBER '%d':\n\n\n",++count);
+	for (i=1; i<=n; i++)
 		printf("\t%d",i);
 
-	for (int i=1; i<=n; i++) {
+	for (i=1; i<=n; i++) {
 		printf("\n\n%d",i);
-		for (int j=1; j<=n; j++) {
+		for (j=1; j<=n; j++) {
+			if (board[i] == j) 
+				printf("\tQ");
+			else
+				printf("\t-");
+		}
+	}
+	printf("\n");
+}
+
+void displayBackTrack(int board[], int n) {
+	int i,j;
+	printf("\n\n\nBACKTRACK NUMBER '%d':\n\n\n",++backTrack);
+	for (i=1; i<=n; i++)
+		printf("\t%d",i);
+
+	for (i=1; i<=n; i++) {
+		printf("\n\n%d",i);
+		for (j=1; j<=n; j++) {
 			if (board[i] == j) 
 				printf("\tQ");
 			else
@@ -22,7 +41,8 @@ void display(int board[], int n) {
 }
 
 int place(int board[], int row, int col) {
-	for (int i=1; i<=row-1; i++) {
+	int i;
+	for (i=1; i<=row-1; i++) {
 		if ((board[i] == col) || (abs(board[i]-col) == (abs(i-row))))
 			return 0;
 	}
@@ -30,20 +50,24 @@ int place(int board[], int row, int col) {
 }
 
 void NQueens(int board[], int row, int n) {
-	for (int i=1; i<=n; i++) {
+	int i; //flag = 0;
+	for (i=1; i<=n; i++) {
 		if (place(board,row,i)) {
 			board[row] = i;	
 			if (row == n) 
 				display(board,n);
-			else
+			else {
 				NQueens(board,row+1,n);
+				//flag = 1;
+			}
 		}
 	}
+	displayBackTrack(board,n);
 }
 
 int main() {
 	int n, board[SIZE];
-	printf("\nN QUEENS PROBLEM -->\n");
+	printf("\nN QUEENS PROBLEM WITH BACKTRACKING -->\n");
 	printf("\nNO. OF QUEENS: ");
 	scanf("%d",&n);
 	while (n < 4) {
